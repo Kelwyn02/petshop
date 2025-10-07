0c1ddbde2d0a19621c855522025a25d0fe4b43b6
Kelwyn02 <kelwyn12345@hotmail.com>
2025-10-07 20:20:18 -0300

    AppointmentData schema

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import z from 'zod';
 import { Button } from './ui/button';
 import {
   Dialog,
@@ -9,20 +10,56 @@ import {
   DialogTitle,
   DialogTrigger,
 } from './ui/dialog';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form } from './ui/form';
+
+const appointmentFormSchema = z.object({
+  tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
+  petName: z.string().min(11, 'O nome do pet é obrigatório'),
+  phone: z.string().min(3, 'O telefone é obrigatório'),
+  description: z.string().min(3, 'A descrição é obrigatória'),
+});
+
+type appointmentFormValues = z.infer<typeof appointmentFormSchema>;
 
 export function AppointmentForm() {
+  const form = useForm<appointmentFormValues>({
+    resolver: zodResolver(appointmentFormSchema),
+    defaultValues: {
+      tutorName: '',
+      petName: '',
+      phone: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = (data: appointmentFormValues) => {
+    console.log(data);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="brand">Novo Agendamento</Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent
+        variant="appointment"
+        overlayVariant="blurred"
+        showCloseButton
+      >
         <DialogHeader>
-          <DialogTitle>Agende um atendimento</DialogTitle>
-          <DialogDescription>
+          <DialogTitle size="modal">Agende um atendimento</DialogTitle>
+          <DialogDescription size="modal">
             Preencha os dados do cliente para realizar o agendamento:
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <input {...form.register('tutorName')} type="text" />
+            <Button type="submit">Salvar</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
